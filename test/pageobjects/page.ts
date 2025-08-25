da9006c1c5e2e6be61f8a2f5f64f881b6f8e9669
Rodolfo Rodrigues <142507322+rodolfor-santos@users.noreply.github.com>
2025-08-25 20:49:22 -0300

    Changes

--- test/pageobjects/page.ts
+++ test/pageobjects/page.ts
@@ -9,7 +9,7 @@ export default class Page {
     * Opens a sub page of the page
     * @param path path of the sub page (e.g. /path/to/page.html)
     */
-    async open (path: string):Promise<string> {
-        return browser.url(path)
+    async open (path: string): Promise<void> {
+        await browser.url(path)
     }
 }
